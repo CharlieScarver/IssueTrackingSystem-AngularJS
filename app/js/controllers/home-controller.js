eaafdd7223a359eddc44aeea54e72d2b5a2464a0
CharlieScarver <s.apostolovd@gmail.com>
2016-05-06 05:01:17 +0300

    Dashboard issues custom made pagination \*.*/; Dashboard affiliated projects - $scope won't update

--- app/js/controllers/home-controller.js
+++ app/js/controllers/home-controller.js
@@ -1,17 +1,19 @@
 'use strict';
 
 angular.module('issueTrackingSystem.home', [
+		//'ui.bootstrap',
 		'issueTrackingSystem.users.authentication',
 		'issueTrackingSystem.users.identity',
 		'issueTrackingSystem.users.userIssues',
-		'issueTrackingSystem.users.userLeadProjects'
+		'issueTrackingSystem.users.userLeadProjects',
+		'issueTrackingSystem.filters.range'		
 	])
 	.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider.when('/', {
 			templateUrl: 'views/home.html',
 			controller: 'HomeController'
 		})
-	}])
+	}])	
 	.controller('HomeController', [
 		'$scope',
 		'$route',
@@ -19,7 +21,12 @@ angular.module('issueTrackingSystem.home', [
 		'identity',
 		'userIssues',
 		'userLeadProjects',
-		function HomeController($scope, $route, authentication, identity, userIssues, userLeadProjects) {
+		function HomeController($scope, $route, authentication, 
+			identity, userIssues, userLeadProjects) {
+			var affiliatedProjects = [],
+				totalIssuePages,
+				currentIssuePage = 1;
+
 			$scope.isAuthenticated = authentication.isAuthenticated();
 
 			$scope.login = function (user) {
@@ -42,19 +49,74 @@ angular.module('issueTrackingSystem.home', [
 				$route.reload();
 			};
 
+			$scope.getIssuesPage = function (page) {
+				userIssues.getUserIssues(page)
+						.then(function(issuesData){
+							$scope.userIssues = issuesData.Issues;
+							$scope.totalIssuePages = issuesData.TotalPages;
+							$scope.currentIssuePage = page;
+						});
+			}
+
+/*/------------------------------- TODO: REMOVE ui.bootstrap and angular-animate
+			$scope.totalItems = 64;
+			$scope.currentPage = 4;
+
+			$scope.setPage = function (pageNo) {
+				$scope.currentPage = pageNo;
+			};
+
+			$scope.pageChanged = function() {
+				$log.log('Page changed to: ' + $scope.currentPage);
+			};
+
+			$scope.maxSize = 5;
+			$scope.bigTotalItems = 175;
+			$scope.bigCurrentPage = 1;
+//----------------------------------*/
+
+			// get current user
 			identity.getCurrentUser()
 				.then(function (user) {
 					$scope.currentUser = user;
 					$scope.isAuthenticated = true;
 					console.log(user);
+
+					// get issues
+					userIssues.getUserIssues(1)
+						.then(function(issuesData){
+							$scope.userIssues = issuesData.Issues;
+							$scope.totalIssuePages = issuesData.TotalPages;					
+
+							// add issue projects to affiliated projects
+							$scope.userIssues.forEach(function(el) {
+								if (el && !affiliatedProjects[el.Project.Name]) {
+									affiliatedProjects[el.Project.Name] = el.Project;
+								}
+							});
+
+							// get projects where user is leader
+							userLeadProjects.getUserLeadProjects(user.Username, 1)
+								.then(function (projectsData) {
+
+									// add them to affiliated projects
+									projectsData.Projects.forEach(function(el) {
+										if (el && !affiliatedProjects[el.Name]) {
+											affiliatedProjects[el.Name] = el;
+										}
+									});
+	
+									if (!$scope.$$phase) {
+										$scope.$apply(function() {
+											$scope.affiliatedProjects = affiliatedProjects;
+											console.log($scope.affiliatedProjects);
+										});
+									}
+
+									$scope.affiliatedProjects = affiliatedProjects;
+											console.log($scope.affiliatedProjects);
+								});																		
+						});
+					
 				});
-
-			var issuesInfo;
-
-			userIssues.getUserIssues()
-				.then(function(issuesData){
-					issuesInfo = issuesData;
-					$scope.userIssues = issuesData.Issues;					
-				});
-
 		}]);
